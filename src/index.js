eaafc6cfab0e48111296dd0a6f92626e7cf27d5b
samet acar <sametacarsamet@gmail.com>
2021-04-19 09:17:18 +0300

    sass file added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/app.scss';
 import App from './App';
 import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
